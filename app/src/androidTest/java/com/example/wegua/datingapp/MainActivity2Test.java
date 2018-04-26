2c8b580d509ec37906ef89ad5048aa0af795d804
Wenbin Guan <weguan001@gmail.com>
2018-04-26 12:50:59 -0700

    Update Test

--- app/src/androidTest/java/com/example/wegua/datingapp/MainActivity2Test.java
+++ app/src/androidTest/java/com/example/wegua/datingapp/MainActivity2Test.java
@@ -1,7 +1,32 @@
 package com.example.wegua.datingapp;
 
 import static org.junit.Assert.*;
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
 
 public class MainActivity2Test {
+
+    @Rule
+    public ActivityTestRule<MainActivity2> activityTestRule
+            = new ActivityTestRule<MainActivity2>(MainActivity2.class) {
+        @Override
+        protected Intent getActivityIntent() {
+            Intent testIntent = new Intent();
+            testIntent.putExtra(Constants.KEY_NAME, "Stephen Curry");
+            testIntent.putExtra(Constants.KEY_OCCUPTION, "Basketball Player");
+            testIntent.putExtra(Constants.KEY_DESCRIPTION, "He is famous basketball player");
+            testIntent.putExtra(Constants.KEY_AGE, "30");
+            return testIntent;
+        }
+    };
 
 }
